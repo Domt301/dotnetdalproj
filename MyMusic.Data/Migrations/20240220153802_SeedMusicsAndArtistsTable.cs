4b311a6e4aea761a9bbdb4609d91611c61460c40
Leonardo Wildt <Leonardo.Wildt@coxautoinc.com>
2024-02-20 10:47:08 -0500

    format

--- MyMusic.Data/Migrations/20240220153802_SeedMusicsAndArtistsTable.cs
+++ MyMusic.Data/Migrations/20240220153802_SeedMusicsAndArtistsTable.cs
@@ -6,13 +6,13 @@ namespace MyMusic.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-     migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Linkin Park')");
-    migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Iron Maiden')");
-    migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Flogging Molly')");
-    migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Red Hot Chilli Peppers')");
-    
-    migrationBuilder.Sql("INSERT INTO \"Musics\" (\"Name\", \"ArtistId\") Values ('In The End', (SELECT \"Id\" FROM \"Artists\" WHERE \"Name\" = 'Linkin Park'))");
-    migrationBuilder.Sql("INSERT INTO \"Musics\" (\"Name\", \"ArtistId\") Values ('Numb', (SELECT \"Id\" FROM \"Artists\" WHERE \"Name\" = 'Linkin Park'))");
+            migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Linkin Park')");
+            migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Iron Maiden')");
+            migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Flogging Molly')");
+            migrationBuilder.Sql("INSERT INTO \"Artists\" (\"Name\") Values ('Red Hot Chilli Peppers')");
+
+            migrationBuilder.Sql("INSERT INTO \"Musics\" (\"Name\", \"ArtistId\") Values ('In The End', (SELECT \"Id\" FROM \"Artists\" WHERE \"Name\" = 'Linkin Park'))");
+            migrationBuilder.Sql("INSERT INTO \"Musics\" (\"Name\", \"ArtistId\") Values ('Numb', (SELECT \"Id\" FROM \"Artists\" WHERE \"Name\" = 'Linkin Park'))");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
